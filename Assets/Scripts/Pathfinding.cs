39566fb8197c3be8d7c0542167423ec33441d65f
Luke Hirst <521480@chrome.escg.ac.uk>
2023-10-13 16:06:29 +0100

    YAY! I know AI!

---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinding : MonoBehaviour
+{
+    public Transform[] points;
+    private NavMeshAgent agent;
+    private int dest;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();    
+    }
+
+    private void FixedUpdate()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GoToNextPoint();
+        }
+    }
+
+    void GoToNextPoint()
+    {
+        if (points.Length == 0)
+        {
+            return;
+        }
+        agent.destination = points[dest].position;
+        dest = (dest + 1) % points.Length;
+    }
+}
